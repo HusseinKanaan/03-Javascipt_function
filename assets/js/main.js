080181fbfe03ca8342a20270f2d769f50009e248
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-07-18 16:09:38 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+console.log('it works')
+// lev1
+
+console.log('Hello world')
+function intro() {
+    console.log('1+3=' + a)
+
+}
+let a = 1 + 3
+
+intro()
+
+// ------lev2-------
+
+function intro2(paramName) {
+
+
+
+    let varName = "SuperCoder"
+
+    console.log("hi, " + varName + ". Mein Name ist " + paramName + " .")
+}
+let name = 'Hussein'
+intro2 = (name)
+
+// ------lev3---------
+
+function intro3 (name, stadt, alter){
+
+    console.log ("Hallo mein Name ist " + name +  ". Ich bin " + Age + " Jahre alt. Ich komme aus " + city + "." )
+}
+
+    
+    let Age = 27
+    let city = "Hamburg"
+
+intro3(name, Age, city)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
